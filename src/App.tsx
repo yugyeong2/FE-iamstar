46cdd424cc1cc23032cf5ee7c69ec82e3db98532
Yugyeong Park <bonbonly7@naver.com>
2024-07-04 23:08:03 +0900

    App에 로그인, 회원가입 엔드포인트 추가, 기본 페이지를 signin 페이지로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
-import ExplorePage from './pages/Explore';
+import ExplorePage from './pages/ExplorePage';
 import MessagesPage from './pages/MessagesPage';
 import CreatePostPage from './pages/CreatePostPage';
-// import CollectionPage from './pages/CollectionPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/messages" element={<MessagesPage />} />
-            <Route path="/create" element={<CreatePostPage />} />
-            {/* <Route path="/Collection" element={<CollectionPage />} /> */}
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/messages" element={<MessagesPage />} />
+          <Route path="/create" element={<CreatePostPage />} />
+        </Routes>
       </div>
     </Router>
   );
