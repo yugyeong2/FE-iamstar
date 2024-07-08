85756241087b59c73f4d54be36103bde18b33fd2
Yugyeong Park <bonbonly7@naver.com>
2024-07-08 10:30:36 +0900

    Home은 component가 아닌 page이므로 네이밍 변경, 로그 추가

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+import { Container } from '@mui/material';
+import Post from '../components/Post';
+import LeftNavBar from '../components/LeftNavBar';
+import ActivityStatus from '../components/ActivityState';
+import './styles/HomePage.css';
+
+interface Comment {
+  userId: string;
+  comment: string;
+  timestamp: string;
+}
+
+interface PostData {
+  id: string;
+  userId: string;
+  username: string;
+  fullName: string;
+  content: string;
+  profileUrl: string;
+  postUrl: string;
+  likes: number;
+  comments: Comment[];
+  timestamp: string;
+}
+
+const HomePage = () => {
+  const [posts, setPosts] = useState<PostData[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/post');
+        if (Array.isArray(response.data)) {
+          setPosts(response.data);
+        } else {
+          throw new Error('API 응답이 배열이 아닙니다.');
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+          console.error('Error fetching posts:', err.message);
+        } else {
+          setError('An unexpected error occurred');
+          console.error('Error fetching posts:', err);
+        }
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="homepage-container">
+      <LeftNavBar />
+      <div className="content-container">
+        <Container className="mt-5">
+          {error && <div className="text-red-500">Error: {error}</div>}
+          <div className="grid grid-cols-1 gap-4">
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                id={post.id}
+                userId={post.userId}
+                username={post.username}
+                fullName={post.fullName}
+                content={post.content}
+                profileUrl={post.profileUrl}
+                postUrl={post.postUrl}
+                likes={post.likes}
+                comments={post.comments}
+                timestamp={post.timestamp}
+              />
+            ))}
+          </div>
+        </Container>
+      </div>
+      <ActivityStatus />
+    </div>
+  );
+};
+
+export default HomePage;
